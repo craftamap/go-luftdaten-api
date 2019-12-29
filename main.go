738a7978b2e3aa95099e6ba93542915895249d30
Fabian Siegel <fabian.siegel@hs-augsburg.de>
2019-12-29 16:40:43 +0100

    Added command line arguments, Request only works with Post

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	ds "github.com/craftamap/go-luftdaten-api/datastructs"
+	flag "github.com/spf13/pflag"
 	"log"
 	"net/http"
 	"os"
@@ -17,23 +19,32 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 }
 
 func main() {
+	port := flag.IntP("port", "p", 8080, "port of web-server")
+	addr := flag.String("address", "0.0.0.0", "address of web-server")
+	csvFile := flag.StringP("outputfile", "o", "", "file to output to")
+	flag.Parse()
+
 	http.HandleFunc("/api/post", logging(
 		func(w http.ResponseWriter, r *http.Request) {
-			var sData ds.SensorData
-			json.NewDecoder(r.Body).Decode(&sData)
+			if r.Method == "POST" {
+				var sData ds.SensorData
+				json.NewDecoder(r.Body).Decode(&sData)
 
-			f, _ := os.OpenFile("data.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
-			defer f.Close()
+				f, _ := os.OpenFile(*csvFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+				defer f.Close()
 
-			csvW := csv.NewWriter(f)
-			defer csvW.Flush()
+				csvW := csv.NewWriter(f)
+				defer csvW.Flush()
 
-			flattenedData := sData.FlattenToArray()
-			log.Printf("%s", flattenedData)
+				flattenedData := sData.FlattenToArray()
+				log.Printf("%s", flattenedData)
 
-			csvW.Write(flattenedData)
+				csvW.Write(flattenedData)
+			} else {
+				http.Redirect(w, r, r.URL.Hostname(), 301)
+			}
 		}),
 	)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", *addr, *port), nil)
 }
